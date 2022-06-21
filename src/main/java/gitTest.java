5aac94b1cf6c05e202ed420e20ea5f3401bd60c0
feifan <2897933880@qq.com>
2022-06-21 16:47:58 +0800

    push commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("hello git3");
         System.out.println("hello master");
         System.out.println("hello git4");
+        System.out.println("hello push");
     }
 }
